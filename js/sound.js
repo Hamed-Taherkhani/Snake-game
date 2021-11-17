0c173ff912e9e2cef3430e8e193873843fc19367
Hamed Taherkhani <hamedtahi7@gmail.com>
2021-11-18 00:00:49 +0330

    Adding turning sound on and off. Changing key press audio. and improving sound functionality

--- js/sound.js
+++ js/sound.js
@@ -1,23 +1,39 @@
 const eatAppleAudio = document.getElementById("eating-apple-audio");
 const keyPressAudio = document.getElementById("click-audio");
-const gameOverAudio = document.getElementById("game-over-audio")
+const gameOverAudio = document.getElementById("game-over-audio");
+
+const soundStateBtn = document.querySelector("#sound-state button");
+const soundOnButton = document.getElementById("sound-on-icon");
+const soundOffButton = document.getElementById("sound-off-icon");
 
 export function playEatingAppleSound() {
     eatAppleAudio.play();
-    setTimeout(() => {
-        eatAppleAudio.pause();
-        eatAppleAudio.load();
-    }, 200);
 }
 
 export function playKeyPressAudio() {
     keyPressAudio.play();
-    setTimeout(() => {
-        keyPressAudio.pause();
-        keyPressAudio.load();
-    }, 300);
 }
 
 export function playGameOverAudio() {
     gameOverAudio.play();
+}
+
+// To turn on or off the sounds of game.
+let isSoundOn = true;
+soundStateBtn.onclick = () => {
+    if (isSoundOn === true) {
+        eatAppleAudio.volume = 0;
+        keyPressAudio.volume = 0;
+        gameOverAudio.volume = 0;
+        soundOnButton.style.display = "none";
+        soundOffButton.style.display = "inline-block";
+        isSoundOn = false;
+    } else {
+        eatAppleAudio.volume = 1;
+        keyPressAudio.volume = 1;
+        gameOverAudio.volume = 1;
+        soundOnButton.style.display = "inline-block";
+        soundOffButton.style.display = "none";
+        isSoundOn = true;
+    }
 }
